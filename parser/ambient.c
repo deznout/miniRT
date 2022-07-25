e16a4d8e94271b1daa3897183559f15bc0f61ce1
Hoover Brent <hbrent@va-i1.kzn.21-school.ru>
2022-07-25 21:55:55 +0300

    normineted

--- parser/ambient.c
+++ parser/ambient.c
@@ -4,6 +4,25 @@
 
 #include "parser.h"
 
+static  int    ambient_final(t_scene *scene)
+{
+    int i;
+
+    i = 0;
+    if (!(scene->ambient.light_height_ratio >= 0.0 && scene->ambient.light_height_ratio <= 1.0))
+        return (INCORRECT);
+    while (scene->ambient.r_g_b[i])
+    {
+        if (!(scene->ambient.r_g_b[i] >= 0 && scene->ambient.r_g_b[i] <= 255))
+            return (INCORRECT);
+        i++;
+    }
+    scene->ambient.identifier_num += 1; // after checking that all params are correct -> then align a value
+    if (scene->ambient.identifier_num != 1)
+        return (INCORRECT);
+    return (CORRECT);
+}
+
 int check_ambient(char **elem_content, t_scene *scene)
 {
     char    **colors;
@@ -14,25 +33,20 @@ int check_ambient(char **elem_content, t_scene *scene)
         i++;
     if (i == 3)
     {
-        i = 0;
         if (check_float(elem_content[1]))
             scene->ambient.light_height_ratio = ft_atof(elem_content[1]);
         else
             return (INCORRECT);
-        if (!(scene->ambient.light_height_ratio >= 0.0 && scene->ambient.light_height_ratio <= 1.0))
-            return (INCORRECT);
         colors = ft_split(elem_content[2], ',');
+        i = 0;
         while (colors[i])
         {
             if (i > 2)
                 return (INCORRECT);
             scene->ambient.r_g_b[i] = ft_atoi(colors[i]);
-            if (!(scene->ambient.r_g_b[i] >= 0 && scene->ambient.r_g_b[i] <= 255))
-                return (INCORRECT);
             i++;
         }
-        scene->ambient.identifier_num += 1; // after checking that all params are correct -> then align a value
-        return (CORRECT);
+        return (ambient_final(scene));
     }
     return (INCORRECT);
 }
--- parser/camera.c
+++ parser/camera.c
@@ -4,6 +4,63 @@
 
 #include "parser.h"
 
+static int  final_camera(t_scene *scene)
+{
+    int i;
+
+    i = 0;
+    while (scene->camera.orient_vector[i])
+    {
+        if (!(scene->camera.orient_vector[i] >= -1 && scene->camera.orient_vector[i] <= 1))
+            return (INCORRECT);
+        i++;
+    }
+    if (!(scene->camera.fov_in_degrees >= 0 && scene->camera.fov_in_degrees <= 180))
+        return (INCORRECT);
+    scene->camera.identifier_num += 1; // after checking that all params are correct -> then align a value
+    if (scene->camera.identifier_num != 1)
+        return (INCORRECT);
+    return (CORRECT);
+}
+
+static void get_camera_cords(int *flag, t_scene *scene, char **elem_content, int num) // correct !!!
+{
+    int     i;
+    char    **v_points;
+
+    i = 0;
+    if (num)
+    v_points = ft_split(elem_content[1], ',');
+    while (v_points[i])
+    {
+        if (i > 2)
+        {
+            *flag = INCORRECT;
+            return ;
+        }
+        if (check_float(v_points[i]))
+            scene->camera.point_xyz[i] = ft_atof(v_points[i]);
+        else
+            *flag = INCORRECT;
+        i++;
+    }
+    v_points = ft_split(elem_content[2], ',');
+    i = 0;
+    while (v_points[i])
+    {
+        if (i > 2)
+        {
+            *flag = INCORRECT;
+            return ;
+        }
+        if (check_float(v_points[i]))
+            scene->camera.orient_vector[i] = ft_atof(v_points[i]);
+        else
+            *flag = INCORRECT;
+        i++;
+    }
+}
+
 int check_camera(char **elem_content, t_scene *scene)
 {
     int     i;
@@ -36,15 +93,10 @@ int check_camera(char **elem_content, t_scene *scene)
                 scene->camera.orient_vector[i] = ft_atof(v_points[i]);
             else
                 return (INCORRECT);
-            if (!(scene->camera.orient_vector[i] >= -1 && scene->camera.orient_vector[i] <= 1))
-                return (INCORRECT);
             i++;
         }
         scene->camera.fov_in_degrees = ft_atoi(elem_content[3]);
-        if (!(scene->camera.fov_in_degrees >= 0 && scene->camera.fov_in_degrees <= 180))
-            return (INCORRECT);
-        scene->camera.identifier_num += 1; // after checking that all params are correct -> then align a value
-        return (CORRECT);
+        return (final_camera(scene));
     }
     return (INCORRECT);
 }
